5a6ece3998327f5175d59cfda05e5ed42cfd659d
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-02 22:52:49 +0900

    Using Map

--- src/main/java/com/example/LambdaMethodHandler.java
+++ src/main/java/com/example/LambdaMethodHandler.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class LambdaMethodHandler {
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
+    public String handleRequest(Map<String, Object> input, Context context) {
+    	LambdaLogger logger = context.getLogger();
+    	logger.log("Input: " + input);
         return "Hello World - " + input;
     }
 }
