9dc7caac991c5755e94696c33e15524d304449f3
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-02 22:47:23 +0900

    Simple Handler

--- src/main/java/com/example/LambdaMethodHandler.java
+++ src/main/java/com/example/LambdaMethodHandler.java
@@ -1,27 +1,10 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.util.stream.Collectors;
-
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-public class LambdaMethodHandler implements RequestStreamHandler {
-	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-			PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8"))))) {
-			String input = reader.lines().collect(Collectors.joining());
-			System.out.println(input);
-			writer.append("Hello World - " + input);
-			writer.flush();
-		}
-	}
+public class LambdaMethodHandler {
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
 }
